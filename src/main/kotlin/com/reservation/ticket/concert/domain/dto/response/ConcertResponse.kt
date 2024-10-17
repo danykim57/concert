52a0c68bd4e15e1206a715f259d33f427d7167a1
YeonginKim <fhtmah12@gmail.com>
2024-10-18 08:31:51 +0900

    chore: 안쓰이는 클래스 정리

--- src/main/kotlin/com/reservation/ticket/concert/domain/dto/response/ConcertResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.reservation.ticket.concert.domain.dto.response
-
-import com.reservation.ticket.concert.domain.dto.ConcertDto
-
-data class ConcertResponse(
-    val code: String,
-    val concerts: List<ConcertDto>,
-)
--- src/main/kotlin/com/reservation/ticket/concert/domain/dto/response/SeatResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.reservation.ticket.concert.domain.dto.response
-
-data class SeatResponse(
-    val code: String,
-    val seats: List<String>,
-)
