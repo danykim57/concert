ef2938fea320d457a82fc410b4074e80d5feaafe
YeonginKim <fhtmah12@gmail.com>
2024-10-18 14:42:38 +0900

    feat: 토큰 발급, 예약 가능 좌석 조회 수정

--- src/main/kotlin/com/reservation/ticket/concert/interfaces/api/token/TokenController.kt
+++ src/main/kotlin/com/reservation/ticket/concert/interfaces/api/token/TokenController.kt
@@ -1,5 +1,6 @@
 package com.reservation.ticket.concert.interfaces.api.token
 
+import com.reservation.ticket.concert.application.facade.QueueFacade
 import com.reservation.ticket.concert.application.service.ConcertService
 import com.reservation.ticket.concert.application.service.QueueService
 import com.reservation.ticket.concert.application.service.UserService
@@ -14,21 +15,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController(value = "/api/token")
 class TokenController(
-    private val userService: UserService,
-    private val queueService: QueueService,
-    private val concertService: ConcertService,
+    private val queueFacade: QueueFacade
 ) {
 
     @Operation(summary = "토큰 발급 신청", description = "토큰 발급 신청 API")
     @PostMapping
-    fun getToken(@RequestBody req: TokenRequest, response: HttpServletResponse): TokenResponse {
-        val user = userService.get(req.id, req.password)
-        val concert = concertService.get(req.concertCode)
-        val token = queueService.createQueue(user, concert)
-        return TokenResponse(
-            code = "success",
-            token = token,
-        )
+    fun getToken(@RequestBody req: TokenRequest): TokenResponse {
+        return queueFacade.getToken(req)
     }
 
 }
