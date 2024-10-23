00db1f647c50d2a5c6b944c727dd5f51bdf5d7f5
YeonginKim <fhtmah12@gmail.com>
2024-10-23 22:48:15 +0900

    refactor: 대기열 큐 검증 로직 합병 2개 -> 1개. QueueService와 QueueStatusChecker에 흩어져 있던 검증 함수 QueueStatusChecker로 이동 및 병합

--- src/main/kotlin/com/reservation/ticket/concert/application/facade/BookingFacade.kt
+++ src/main/kotlin/com/reservation/ticket/concert/application/facade/BookingFacade.kt
@@ -1,7 +1,6 @@
 package com.reservation.ticket.concert.application.facade
 
 import com.reservation.ticket.concert.application.service.ConcertService
-import com.reservation.ticket.concert.application.service.QueueService
 import com.reservation.ticket.concert.application.service.ReservationService
 import com.reservation.ticket.concert.application.service.SeatService
 import com.reservation.ticket.concert.application.token.QueueStatusChecker
@@ -9,7 +8,7 @@ import com.reservation.ticket.concert.domain.Reservation
 import com.reservation.ticket.concert.domain.ReservationStatus
 import com.reservation.ticket.concert.domain.dto.SeatDTO
 import com.reservation.ticket.concert.domain.toDto
-import com.reservation.ticket.concert.infrastructure.ReservationRepository
+import com.reservation.ticket.concert.infrastructure.exception.UnprocessableEntityException
 import com.reservation.ticket.concert.interfaces.request.BookingRequest
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
@@ -18,7 +17,6 @@ import java.util.*
 @Service
 class BookingFacade(
     private val seatService: SeatService,
-    private val queueService: QueueService,
     private val queueStatusChecker: QueueStatusChecker,
     private val concertService: ConcertService,
     private val reservationService: ReservationService
@@ -26,23 +24,18 @@ class BookingFacade(
 
     @Transactional
     fun book(request: BookingRequest, userId: UUID): SeatDTO {
-        if (queueStatusChecker.isQueueStatusPass(userId) == false)
-            throw IllegalArgumentException("아직 대기중입니다.")
-
-        if (!queueService.isValidToken(request.queueToken)) {
-            throw IllegalArgumentException("유효하지 않은 대기열 토큰입니다.")
-        }
+        queueStatusChecker.checkTokenValid(request.queueToken, userId)
 
         val concert = concertService.get(request.concertId)
         if (concert.isEmpty)
-            throw IllegalArgumentException("없는 콘서트 입니다.")
+            throw UnprocessableEntityException("없는 콘서트 입니다.")
 
         val seat = seatService.get(request.seatId)
-            ?: throw IllegalArgumentException("해당 좌석이 존재하지 않습니다.")
+            ?: throw UnprocessableEntityException("해당 좌석이 존재하지 않습니다.")
 
 
         if (!seat.isAvailable) {
-            throw IllegalStateException("해당 좌석은 이미 예약되었습니다.")
+            throw UnprocessableEntityException("해당 좌석은 이미 예약되었습니다.")
         }
 
         seat.isAvailable = false
--- src/main/kotlin/com/reservation/ticket/concert/application/token/QueueStatusChecker.kt
+++ src/main/kotlin/com/reservation/ticket/concert/application/token/QueueStatusChecker.kt
@@ -2,19 +2,24 @@ package com.reservation.ticket.concert.application.token
 
 import com.reservation.ticket.concert.domain.QueueStatus
 import com.reservation.ticket.concert.infrastructure.QueueRepository
+import com.reservation.ticket.concert.infrastructure.exception.ForbiddenException
+import com.reservation.ticket.concert.infrastructure.exception.UnprocessableEntityException
 import org.springframework.stereotype.Component
-import java.util.*
+import java.util.UUID
 
 @Component
 class QueueStatusChecker(
     private val queueRepository: QueueRepository
 ) {
+    fun checkTokenValid(token: String, userId: UUID) {
+        if(token.isEmpty()) throw ForbiddenException("유효하지 않은 대기열 토큰입니다.")
+        isQueueStatusPass(userId)
+    }
 
     // UserId로 Queue 테이블에서 상태가 PASS인지 확인하는 메서드
     fun isQueueStatusPass(userId: UUID): Boolean {
         val queue = queueRepository.findByUserId(userId)
-            ?: throw IllegalArgumentException("해당 유저의 대기열이 존재하지 않습니다.")
-
+            ?: throw UnprocessableEntityException("아직 대기중입니다.")
         return queue.status == QueueStatus.PASS
     }
 }
