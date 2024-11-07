24d18050b66a45180d40629a1ccd3b9aa236b6ca
YeonginKim <fhtmah12@gmail.com>
2024-11-08 07:00:28 +0900

    env: EnableCaching 활성화

--- src/main/kotlin/com/reservation/ticket/concert/ConcertApplication.kt
+++ src/main/kotlin/com/reservation/ticket/concert/ConcertApplication.kt
@@ -2,12 +2,14 @@ package com.reservation.ticket.concert
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableCaching
 class ConcertApplication
 
 fun main(args: Array<String>) {
