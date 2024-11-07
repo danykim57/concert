09e28ef87a9273d21bcecfba22f27074681b3333
YeonginKim <fhtmah12@gmail.com>
2024-11-08 07:01:01 +0900

    feat: 예약 가능한 콘서트 조회 캐시 적용

--- src/main/kotlin/com/reservation/ticket/concert/application/service/ConcertService.kt
+++ src/main/kotlin/com/reservation/ticket/concert/application/service/ConcertService.kt
@@ -3,23 +3,41 @@ package com.reservation.ticket.concert.application.service
 import com.reservation.ticket.concert.domain.Concert
 import com.reservation.ticket.concert.infrastructure.ConcertRepository
 import com.reservation.ticket.concert.infrastructure.exception.UnprocessableEntityException
+import org.springframework.cache.CacheManager
 import org.springframework.stereotype.Service
 import java.util.Optional
 
 @Service
 class ConcertService(
     private val concertRepository: ConcertRepository,
+    private val cacheManager: CacheManager,
 ) {
     fun get(name: String): Concert = concertRepository.findByName(name)
         ?: throw UnprocessableEntityException("유효하지 않은 콘서트 입니다.")
 
     fun get(id: Long): Optional<Concert> = concertRepository.findById(id)
 
-    fun create(concert: Concert): Concert = concertRepository.save(concert)
+    fun create(concert: Concert): Concert {
+        val savedConcert = concertRepository.save(concert)
+        val cache = cacheManager.getCache("concert")
+        cache?.put(savedConcert.name, savedConcert)
+       return savedConcert
+    }
 
     fun getAvailableConcerts(): List<Concert> {
-        // 예약 가능한 콘서트를 리포지토리에서 조회
-        return concertRepository.findAvailableConcerts()
+
+        val cache = cacheManager.getCache("concert")
+        val cachedConcerts = cache?.get("availableConcerts", List::class.java) as? List<Concert>
+
+        if (cachedConcerts != null) {
+            return cachedConcerts
+        }
+
+        // 캐시에 없으면 DB에서 조회 후 캐시에 저장
+        val availableConcerts = concertRepository.findAvailableConcerts()
+        cache?.put("availableConcerts", availableConcerts)
+
+        return availableConcerts
     }
 
 }
--- src/main/kotlin/com/reservation/ticket/concert/infrastructure/ConcertRepository.kt
+++ src/main/kotlin/com/reservation/ticket/concert/infrastructure/ConcertRepository.kt
@@ -1,6 +1,7 @@
 package com.reservation.ticket.concert.infrastructure
 
 import com.reservation.ticket.concert.domain.Concert
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.util.Optional
@@ -8,6 +9,7 @@ import java.util.Optional
 interface ConcertRepository : JpaRepository<Concert, Long> {
 
     // 예약 가능한 콘서트만 조회하는 쿼리
+    @Cacheable(value = ["availableConcerts"], cacheManager = "redisCacheManager")
     @Query("SELECT c FROM Concert c WHERE c.availableTickets > 0 AND c.date > CURRENT_TIMESTAMP")
     fun findAvailableConcerts(): List<Concert>
 
