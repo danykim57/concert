b25f60040e14927aa1cd943de786e8f4ecfd78d1
YeonginKim <fhtmah12@gmail.com>
2024-11-08 06:59:53 +0900

    env: Redis, 캐시 세팅

--- src/main/kotlin/com/reservation/ticket/concert/config/RedisConfig.kt
+++ src/main/kotlin/com/reservation/ticket/concert/config/RedisConfig.kt
@@ -1,13 +1,17 @@
 package com.reservation.ticket.concert.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.utility.DockerImageName
+
 
 @Configuration
 class RedisConfig {
@@ -26,11 +30,13 @@ class RedisConfig {
     }
 
     @Bean
-    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
-        val template = RedisTemplate<String, String>()
-        template.connectionFactory = connectionFactory
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val objectMapper = ObjectMapper()
+        objectMapper.registerModule(JavaTimeModule())
+        val template = RedisTemplate<String, Any>()
+        template.setConnectionFactory(connectionFactory)
         template.keySerializer = StringRedisSerializer()
-        template.valueSerializer = StringRedisSerializer()
+        template.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
         return template
     }
 }
