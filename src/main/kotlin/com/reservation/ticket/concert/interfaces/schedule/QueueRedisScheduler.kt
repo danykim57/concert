7c5850314decc6adc3ca0319c53c6b71b0ec173f
YeonginKim <fhtmah12@gmail.com>
2024-11-08 11:11:13 +0900

    fix: 스케쥴러 수정

--- src/main/kotlin/com/reservation/ticket/concert/interfaces/schedule/QueueRedisScheduler.kt
+++ src/main/kotlin/com/reservation/ticket/concert/interfaces/schedule/QueueRedisScheduler.kt
@@ -1,35 +1,41 @@
 package com.reservation.ticket.concert.interfaces.schedule
 
 import com.reservation.ticket.concert.application.service.QueueService
+import com.reservation.ticket.concert.domain.QueueStatus
 import org.slf4j.LoggerFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class QueueRedisScheduler(
     private val queueService: QueueService,
     private val redisTemplate: RedisTemplate<String, String>
 ) {
-    private val logger = LoggerFactory.getLogger(QueueScheduler::class.java)
+    private val logger = LoggerFactory.getLogger(QueueRedisScheduler::class.java)
 
-    // 10초마다 실행
-    @Scheduled(fixedRate = 10000)
+    // 1분마다 실행
+    @Scheduled(fixedRate = 60000)
     fun activateNextBatch() {
         val concertId = "exampleConcert"  // 예제 콘서트 ID
         val queueKey = "concertQueue:$concertId"
 
         // 상위 50명의 사용자 가져오기
-        val usersToActivate = redisTemplate.opsForZSet().range(queueKey, 0, 49)
+        val usersToActivate = redisTemplate.opsForSet().members(queueKey) ?: emptySet()
 
-        if (usersToActivate != null && usersToActivate.isNotEmpty()) {
-            usersToActivate.forEach { userId ->
-                logger.info("Activating user: $userId")
-                // 여기서 각 사용자에 대한 활성화 로직을 수행합니다.
+        if (usersToActivate.isNotEmpty()) {
+            // 상위 50명의 사용자들을 활성화 상태로 변경
+            usersToActivate.take(50).forEach { userId ->
+                val userToken = UUID.fromString(userId)
+                val statusKey = "userStatus:$userToken"
+
+                // 사용자 상태를 WAITING -> ACTIVE로 변경
+                redisTemplate.opsForHash<String, String>().put(statusKey, "status", QueueStatus.PASS.name)
+                logger.info("Activated user: $userToken")
+
+                // 대기열에서 활성화된 사용자 제거는 다른 스케쥴러에서 처리
             }
-
-            // 활성화된 사용자들을 대기열에서 제거
-            redisTemplate.opsForZSet().removeRange(queueKey, 0, 49)
         } else {
             logger.info("No users in queue to activate.")
         }
