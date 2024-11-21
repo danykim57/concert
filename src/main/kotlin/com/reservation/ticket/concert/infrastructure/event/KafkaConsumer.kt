d789495449bf13e948c4822f8a942587b0d53cd3
YeonginKim <fhtmah12@gmail.com>
2024-11-22 00:27:52 +0900

    feat: 카프카 Consumer Producer 클래스

---
+++ src/main/kotlin/com/reservation/ticket/concert/infrastructure/event/KafkaConsumer.kt
@@ -0,0 +1,19 @@
+package com.reservation.ticket.concert.infrastructure.event
+
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+
+@Component
+class KafkaConsumer {
+
+    private val logger = LoggerFactory.getLogger(KafkaConsumer::class.java)
+
+    var receivedMessage: String? = null
+
+    @KafkaListener(topics = ["test-topic"], groupId = "test-group")
+    fun listen(message: String) {
+        logger.info("Received message: $message")
+        receivedMessage = message
+    }
+}
