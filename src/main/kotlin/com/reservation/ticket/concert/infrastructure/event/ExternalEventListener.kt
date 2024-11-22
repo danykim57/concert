e41cba063553ef111abd9bc387f3351242104991
YeonginKim <fhtmah12@gmail.com>
2024-11-22 14:49:15 +0900

    fix: 이벤트 소비자에 Transaction Before-commit After-commit 추가

--- src/main/kotlin/com/reservation/ticket/concert/infrastructure/event/ExternalEventListener.kt
+++ src/main/kotlin/com/reservation/ticket/concert/infrastructure/event/ExternalEventListener.kt
@@ -1,6 +1,7 @@
 package com.reservation.ticket.concert.infrastructure.event
 
 import com.reservation.ticket.concert.infrastructure.external.EventApiClient
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
@@ -10,8 +11,12 @@ import org.springframework.transaction.event.TransactionalEventListener
 class ExternalEventListener(
     private val eventApiClient: EventApiClient
 ) {
+    @KafkaListener(
+        topics = ["reservation-confirm-topic"],
+        containerFactory = "kafkaListenerContainerFactory",
+    )
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     fun handle(event: ReservationEvent) {
         eventApiClient.sendReservationEvent()
     }
